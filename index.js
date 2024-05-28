3cc1ab695e99622ec8bbdfd77b35ab1ed2387295
maab-fw <maabfw@proton.me>
2024-05-28 16:36:17 +0600

    module 70 payment class finished

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 // middleware
@@ -30,6 +31,7 @@ async function run() {
         const menuCollection = client.db("bistroDB").collection("menu")
         const reviewsCollection = client.db("bistroDB").collection("reviews")
         const cartsCollection = client.db("bistroDB").collection("carts")
+        const paymentCollection = client.db("bistroDB").collection("payments")
 
         // jwt related api
         app.post("/jwt", async (req, res) => {
@@ -188,6 +190,44 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await cartsCollection.deleteOne(query)
             res.send(result)
+        })
+
+        // payment intent
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body
+            const amount = parseInt(price * 100)
+            console.log("amount", amount)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
+
+        app.get("/payments/:email", verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            console.log(query);
+            if (req.params.email !== req.decoded.email) {
+                return res.status(403).send({ message: "forbidden access" })
+            }
+            const result = await paymentCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post("/payments", async (req, res) => {
+            const payment = req.body
+            const paymentResult = await paymentCollection.insertOne(payment)
+
+            // carefully delete each item from the cart
+            console.log("payment info", payment)
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map((id) => new ObjectId(id)),
+                },
+            }
+            const deleteResult = await cartsCollection.deleteMany(query)
+            res.send({ paymentResult, deleteResult })
         })
 
         // Send a ping to confirm a successful connection
